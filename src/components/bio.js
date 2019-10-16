bee21c5e14a7927e8a6f79ff4305f79e721c2411
Austin Brunkhorst <AustinBrunkhorst@users.noreply.github.com>
2019-10-15 23:34:13 -0700

    Update bio + CNAME

--- src/components/bio.js
+++ src/components/bio.js
@@ -25,7 +25,7 @@ const Bio = () => {
     }
   `)
 
-  const { author, social } = data.site.siteMetadata
+  const { author } = data.site.siteMetadata
   return (
     <div
       style={{
@@ -51,7 +51,7 @@ const Bio = () => {
           Greetings human! I'm <strong>{author}</strong>, and I like to make all the things.
         </p>
         <p>
-          Follow me on <a href={`https://twitter.com/${social.twitter}`}>Twitter</a> or <a href={`https://github.com/${social.github}`}>GitHub</a>.
+          Follow on <a href={`https://twitter.com/intent/follow?original_referer=${encodeURIComponent(window.location)}&ref_src=twsrc%5Etfw&region=follow_link&screen_name=abrunkhorst&tw_p=followbutton`}>Twitter</a> or <a href="https://github.com/AustinBrunkhorst">GitHub</a>.
         </p>
       </div>
     </div>
