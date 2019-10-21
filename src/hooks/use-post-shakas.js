a7f79099ecf6d24beaacf564ff694b31ad2de1c5
Austin Brunkhorst <AustinBrunkhorst@users.noreply.github.com>
2019-10-20 23:38:50 -0700

    add ga event for shaka

--- src/hooks/use-post-shakas.js
+++ src/hooks/use-post-shakas.js
@@ -1,10 +1,19 @@
 import { useState, useCallback } from "react";
 
-export default function usePostShakas() {
+export default function usePostShakas(slug) {
   const [total, setTotal] = useState(0);
 
   const giveShaka = useCallback(() => {
     setTotal(total + 1);
+
+    if (window && window.ga) {
+      window.ga('send', {
+        hitType: 'event',
+        eventCategory: slug,
+        eventAction: 'send_shaka',
+        eventValue: total
+      });
+    }
   }, [total, setTotal]);
 
   return { total, giveShaka };
